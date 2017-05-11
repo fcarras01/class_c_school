2561ce9a4dede4a3c08b7578af74bca7d7161616
fcarras01 <fcarras01@gmail.com>
2017-05-10 22:34:26 -0400

    New lessons

---
+++ 01_lesson/hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/*main function that has a integer return*/
+/*you must have a main function*/
+
+int main()
+{
+	/* Another hello world, only this time in C. */
+	printf("This is another hello world program only this time in C. \n");
+	printf("Main function that has a integer return. \n");
+	printf("You must have a main function. \n");
+	return 0; /*Sends back something. 0 means once the main function runs completely, it will return a 0, if not it will be greater than 0*/
+}
---
+++ 02_lesson/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() { //open main for readability
+
+	char y = 'x'; //set x to x
+	
+	int a, b, c, d; //create varibales but set them to nothing
+
+	printf("Value of y is actually: %c \n", y);
+
+	return 0;
+
+}//close main for readability
